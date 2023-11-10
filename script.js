6086cf5905ba0925b7a88cd957e77e0fe94a9663
˜codethi˜ <thiago.lima3@ab-inbev.com>
2023-11-09 21:06:47 -0300

    feat: delete

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const produtos = [];
+let id = 0;
 
 const btnCadastrar = document.querySelector("#cadastrar");
+const btnNovoProduto = document.querySelector("#new-product");
 
 btnCadastrar.addEventListener("click", cadastrarProduto);
+btnNovoProduto.addEventListener("click", toggleFormInsert);
 
 function cadastrarProduto() {
+  id += 1;
   const nome = document.querySelector("#nome").value;
   const preco = document.querySelector("#preco").value;
   const quantidade = document.querySelector("#quantidade").value;
 
   const produto = {
+    id,
     nome,
     preco,
     quantidade,
@@ -17,8 +22,7 @@ function cadastrarProduto() {
 
   produtos.push(produto);
 
-  alert(`Produto cadastrado com sucesso!`);
-
+  toggleFormInsert();
   listarProdutos();
 }
 
@@ -31,9 +35,25 @@ function listarProdutos() {
           <td>${produtos[i].nome}</td>
           <td>${produtos[i].preco}</td>
           <td>${produtos[i].quantidade}</td> 
+          <td><button onclick="deleteProduct(${produtos[i].id})">Excluir</button></td>
         </tr> 
     `;
   }
+}
 
-  console.log(produtos);
+function deleteProduct(id) {
+  for (let i = 0; i < produtos.length; i++) {
+    if (produtos[i].id === id) {
+      produtos.splice(i, 1);
+      break;
+    }
+  }
+
+  listarProdutos();
+}
+function toggleFormInsert() {
+  const formInsert = document.querySelector("#form-insert");
+
+  formInsert.classList.toggle("display-none");
+  formInsert.classList.toggle("display-flex");
 }
