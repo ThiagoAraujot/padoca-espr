88f3e40bd2eec93c778863e0221c2d4e0e8e4b7d
˜codethi˜ <thiago.lima3@ab-inbev.com>
2023-11-16 20:04:23 -0300

    feat: update

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const produtos = [];
 let id = 0;
+let foundId;
 
 const btnCadastrar = document.querySelector("#cadastrar");
 const btnNovoProduto = document.querySelector("#new-product");
+const btnAtualizar = document.querySelector("#update");
 
 btnCadastrar.addEventListener("click", cadastrarProduto);
 btnNovoProduto.addEventListener("click", toggleFormInsert);
+btnAtualizar.addEventListener("click", updateProduct);
 
 function cadastrarProduto() {
   id += 1;
@@ -36,6 +39,7 @@ function listarProdutos() {
           <td>${produtos[i].preco}</td>
           <td>${produtos[i].quantidade}</td> 
           <td><button onclick="deleteProduct(${produtos[i].id})">Excluir</button></td>
+          <td><button onclick="getInfoProduct(${produtos[i].id})">Atualizar</button></td>
         </tr> 
     `;
   }
@@ -56,4 +60,42 @@ function toggleFormInsert() {
 
   formInsert.classList.toggle("display-none");
   formInsert.classList.toggle("display-flex");
+}
+
+function toggleFormUpdate() {
+  const formUpdate = document.querySelector("#form-update");
+
+  formUpdate.classList.toggle("display-none");
+  formUpdate.classList.toggle("display-flex");
+}
+
+function getInfoProduct(id) {
+  foundId = id;
+  toggleFormUpdate();
+
+  for (let i = 0; i < produtos.length; i++) {
+    if (produtos[i].id === id) {
+      document.querySelector("#nomeUpdate").value = produtos[i].nome;
+      document.querySelector("#precoUpdate").value = produtos[i].preco;
+      document.querySelector("#quantidadeUpdate").value =
+        produtos[i].quantidade;
+
+      break;
+    }
+  }
+}
+
+function updateProduct() {
+  for (let i = 0; i < produtos.length; i++) {
+    if (produtos[i].id === foundId) {
+      produtos[i].nome = document.querySelector("#nomeUpdate").value;
+      produtos[i].preco = document.querySelector("#precoUpdate").value;
+      produtos[i].quantidade =
+        document.querySelector("#quantidadeUpdate").value;
+      toggleFormUpdate();
+      break;
+    }
+  }
+
+  listarProdutos();
 }
